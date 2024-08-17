49ab57f26461fee46e99eb7e1d741406af5efcfc
Bhagatsakshi <142607822+Bhagatsakshi@users.noreply.github.com>
2024-08-17 11:44:20 +0000

    commit

---
+++ Basics/coversions.js
@@ -0,0 +1,18 @@
+score="33"
+console.log(typeof score);
+
+varNum=Number(score)
+console.log(typeof varNum);
+
+console.log(varNum);
+// "33"=>33
+// score="33abc"==>Nan
+// 1=>true ; 0=>false
+
+let isLoggedIn = " "
+let bool = Boolean(isLoggedIn)
+console.log(bool);
+
+// ""==>false
+// "abc"=>true
+// " "=>true
