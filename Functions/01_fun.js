cd35786ff0cba97ddad7e24b5f832f37432b11fd
Bhagatsakshi <142607822+Bhagatsakshi@users.noreply.github.com>
2025-02-25 08:26:19 +0000

    coomit

---
+++ Functions/01_fun.js
@@ -0,0 +1,32 @@
+// function sayname(){
+//     console.log("abc");   
+// }
+
+// sayname()
+
+//in function defination,numbers are parameters
+function addnumbers(num1,num2){
+    console.log(num1+num2);
+}
+
+addnumbers(2,4)    //when you call,numbers are arguments
+// addnumbers(2,"4") -->24
+// addnumbers(2,"a") -->2a
+
+function subnumbers(num1,num2){
+   let res=num1-num2;
+   return res;
+}
+
+const res=subnumbers(3,4);
+console.log(res);
+
+function loginuser(user="sam"){    //overrides
+    if(!user){
+        console.log("enter username");
+        return
+    }
+    return `${user} just logged in`
+}
+
+console.log(loginuser("hitesh"));
---
+++ Functions/02_fun.js
@@ -0,0 +1,10 @@
+function calculatePrice(...num1){    //rest operator ,wil make arr of prices
+    return num1
+}
+
+// function calculatePrice(V1,V2,...num1){    v1--200,v2--400,num1--300
+//     return num1
+// }
+
+console.log(calculatePrice(200,400,300));
+
